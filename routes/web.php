f592236e8cd0b1050457fc7c2e43a347fd9d21af
Brou kouadio Stéphane-Fabien <fabienbrou99@gmail.com>
2024-08-21 11:08:29 +0000

    les routes

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
+############################
+#   LES ROUTES EN LARAVEL   #
+############################
 Route::get('/', function () {
     return view('welcome');
 });
