5f9818161b96270cfc2f49134d40ac008d131805
Brou kouadio Stéphane-Fabien <fabienbrou99@gmail.com>
2024-08-21 11:12:38 +0000

    changement de la première page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,9 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://cdn.tailwindcss.com"></script>
-    <title>La page Bonjour</title>
+    <title>Première page</title>
 </head>
 <body>
-    <h1 class="text-lg font-bold text-center">Bonjour La famille!</h1>
+    <h1 class="text-lg font-bold text-center">Page d'accueil</h1>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,8 @@ use Illuminate\Support\Facades\Route;
 ############################
 #   LES ROUTES EN LARAVEL   #
 ############################
+
+# route normale
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
