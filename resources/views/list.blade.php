fdff91fd54807bb97600d8fdd36f8b5049f44ddb
Brou kouadio Stéphane-Fabien <fabienbrou99@gmail.com>
2024-08-22 12:27:09 +0000

    affectation d'un controller à une route

--- resources/views/list.blade.php
+++ resources/views/list.blade.php
@@ -10,10 +10,8 @@
     <h1 class="text-lg font-bold text-center">Films</h1>
     <div class="p-5 mx-auto border border-black max-w-[500px]">
         <ul>
-            <!-- route('movies.show') accès à la route avec son nom de route -->
-            <!-- ['title'=>'bamba'] passé les paramètres requis par la route-->
-            <li><a class="hover:text-red-400" href="{{route('movies.show', ['title'=>'bamba'])}}">Bamba</a></li>
-            <li><a class="hover:text-red-400" href="{{route('movies.show', ['title'=>'php'])}}">PHP</a></li>
+            <li><a class="hover:text-red-400" href="#">Bamba</a></li>
+            <li><a class="hover:text-red-400" href="#">PHP</a></li>
         </ul>
     </div>
 </body>
--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,10 @@
 <?php
 
+use App\Http\Controllers\MovieController;
 use Illuminate\Support\Facades\Route;
 
 ############################
 #   LES ROUTES EN LARAVEL   #
 ############################
 
-Route::get('/movies/list', function () {
-    return view('list');
-})->name('movies.list');
-
-## acceder à une route avec paramètre
-Route::get('/movies/{title}', function (string $title) {
-    return "Movie title: $title";
-})->name('movies.show');
+Route::get('/movies/list', [MovieController::class, 'index'])->name('movies.list');
