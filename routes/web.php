d898293ab2efec518e634fdc96626788de84d702
Brou kouadio Stéphane-Fabien <fabienbrou99@gmail.com>
2024-08-22 11:39:33 +0000

    attribution de nom à une route

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,12 @@ use Illuminate\Support\Facades\Route;
 #   LES ROUTES EN LARAVEL   #
 ############################
 
-# route normale
+## route avec une fonction anonyme
 Route::get('/', function () {
     return view('index');
 });
+
+## attribution de nom à une route (php artisan route:list)
+Route::get('/movies/halloween', function () {
+    return "Movie title: Halloween";
+})->name('movies.halloween');
