c616d980712285a29dd5d7e0d54c5b9ad1837343
Brou kouadio Stéphane-Fabien <fabienbrou99@gmail.com>
2024-08-22 11:48:59 +0000

    route avec paramètre

--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,15 @@ Route::get('/', function () {
 Route::get('/movies/halloween', function () {
     return "Movie title: Halloween";
 })->name('movies.halloween');
+
+## route avec paramètre
+# ajout de contrainte sur le paramètre year
+Route::get('/movies/{year}', function (string $year) {
+    return "Movie year: $year";
+})->where('year', '\d{4}');# 4 chiffre
+
+# le paramètre 'title' est requis
+# le paramètre 'genre' est optionnel
+Route::get('/movies/{title}/{genre?}', function (string $title) {
+    return "Movie title: $title";
+});
